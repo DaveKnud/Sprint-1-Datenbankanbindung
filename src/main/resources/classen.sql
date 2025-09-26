48c372cbca5e04969f2345c2ce45d1a726147458
AlexandruMIhaiMatei <alexandru.mihai.matei@web.de>
2025-09-26 10:48:56 +0200

    Sql für tabelle machen

---
+++ src/main/resources/classen.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS Customer;
+DROP TABLE IF EXISTS Gender;
+
+
+-- Table 1: Gender
+CREATE TABLE Gender (
+    GenderID INT PRIMARY KEY AUTO_INCREMENT,
+    Description VARCHAR(50) NOT NULL UNIQUE
+);
+
+-- Table 2: Customer
+CREATE TABLE Customer (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    FirstName VARCHAR(100) NOT NULL,
+    LastName VARCHAR(100) NOT NULL,
+    GenderID INT,
+    BirthDate DATE,
+    FOREIGN KEY (GenderID) REFERENCES Gender(GenderID)
+);
+
+INSERT INTO Gender (Description) VALUES
+('M'),
+('F'),
+('U'),
+('W');
