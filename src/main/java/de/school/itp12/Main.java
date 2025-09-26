92075491feb72850ce348f3113ff3347074dc193
AlexandruMIhaiMatei <alexandru.mihai.matei@web.de>
2025-09-26 10:29:20 +0200

    Initial Commit

--- src/main/java/de/school/itp12/Main.java
+++ src/main/java/de/school/itp12/Main.java
@@ -4,6 +4,6 @@ package de.school.itp12;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("test");
     }
 }
